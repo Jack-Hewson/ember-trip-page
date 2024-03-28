008117fce1faa9c825fc2cf360c329819cdc3b81
Jack Hewson <jack.hewson@taicaan.com>
2024-03-28 11:00:04 +0000

    purchase button style change

--- src/sidebar/PurchaseComponent.tsx
+++ src/sidebar/PurchaseComponent.tsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 
 const PurchaseComponent = () => {
     return (
-        <Button variant='outlined' color="primary" href="https://www.ember.to/" target="_blank">Purchase Tickets </Button>
+        <Button sx={{ position: 'relative' }} variant='outlined' color="primary" href="https://www.ember.to/" target="_blank">Buy</Button>
     )
 }
 
